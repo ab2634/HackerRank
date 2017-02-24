3bb466786f81671d0333b8ae19d81acd330f00bb
Bert Baldoni <ab2634@nova.edu>
2017-02-23 20:59:50 -0500

    commit 2/23/17

--- HackerRankDSStacks/src/dSBalancedBrackets/Solution.java
+++ HackerRankDSStacks/src/dSBalancedBrackets/Solution.java
@@ -12,45 +12,78 @@ import java.util.regex.*;
 /**
  * @author BertBaldoni
  * @version 1.0 Feb 23, 2017
- * @project Solution.java Feb 23, 2017
- * Copyright (c) BertBaldoni 2017 All Rights Reserved
+ * @project Solution.java Feb 23, 2017 Copyright (c) BertBaldoni 2017 All Rights
+ *          Reserved
  *
  */
 public class Solution {
 
-	public static class TriStack {
-		
+	public static class Stack {
+
 		char[] array;
-		boolean[] stackCurly;
-		boolean[] stackParen;
-		boolean[] stackBrack;
-		int pointer;
-		
-		public TriStack(String sequence) {
-			array = new char[1000];
-			stackCurly = new boolean[1000];
-			stackParen = new boolean[1000];
-			stackBrack = new boolean[1000];
-			int pointer = 0;
-			
+		int a, b, c, d, e, f;
+
+		public Stack(String sequence) {
+			array = new char[sequence.length() + 1];
 			array = sequence.toCharArray();
-			
+			a = 0;
+			b = 0;
+			c = 0;
+			d = 0;
+			e = 0;
+			f = 0;
 		}
-	
 		
+		public boolean checkIfBalanced() {
+			for (int i = 0; i < array.length - 1; i++) {
+				char x = array[i];
+				char y = array[i + 1];
+				if ((x == '{' && y == ')') || (x == '{' && y == ']') || (x == '(' && y == '}') || 
+						(x == '(' && y == ']') || (x == '[' && y == ')') || (x == '[' && y == '}'))
+					return false;
+			}
+			return true;
+		}
+		
+		public boolean checkIfEven() {
+			for (int i = 0; i < array.length; i++) {
+				if (array[i] == '{')
+					a++;
+				else if (array[i] == '}')
+					d++;
+				else if (array[i] == '(')
+					b++;
+				else if (array[i] == ')')
+					e++;
+				else if (array[i] == '[')
+					c++;
+				else if (array[i] == ']')
+					f++;
+				else;
+			}
+			if (a == d && b == e && c == f)
+				return true;
+			return false;
+		}	
 	}
-	
+
 	public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        // String sequence = "";
-        
-        TriStack stack = new TriStack(scanner.nextLine());
-        
-        /*
-        for (int i = 0; i < n; i++) {
-        	
-        }
-        */
-    }
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		scanner.nextLine();
+		
+		String[] output = new String[10000];
+		int outputCount = 0;
+		for (int i = 0; i < n; i++) {
+			String sequence = scanner.nextLine();
+			Stack stack = new Stack(sequence);
+			if (stack.checkIfBalanced() == true && stack.checkIfEven() == true)
+				output[i] = "YES";
+			else
+				output[i] = "NO";
+			outputCount++;
+		}
+		for (int i = 0; i < outputCount; i++) 
+			System.out.println(output[i]);
+	}
 }
