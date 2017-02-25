3ae606c844a9c5b23da4959b571767f99ca85000
Bert Baldoni <ab2634@nova.edu>
2017-02-25 17:23:34 -0500

    commit 2/25/17

---
+++ HackerRankDSStacks/src/dSLargestRectangle/Solution.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package dSLargestRectangle;
+
+/**
+ * @author BertBaldoni
+ * @version 1.0 Feb 24, 2017
+ * @project Solution.java Feb 24, 2017
+ * Copyright (c) BertBaldoni 2017 All Rights Reserved
+ *
+ */
+public class Solution {
+
+	public static void main(String[] args) {
+
+	}
+
+}
